9fb21b2c5858aeeadecca8afe4e4945ce0bf87ae
Brais <braisgabin@gmail.com>
2022-04-20 11:34:12 +0200

    WIP

---
+++ src/main/kotlin/com/braisgabin/detekt/junit/UnnecesaryNested.kt
@@ -0,0 +1,37 @@
+package com.braisgabin.detekt.junit
+
+import io.gitlab.arturbosch.detekt.api.CodeSmell
+import io.gitlab.arturbosch.detekt.api.Config
+import io.gitlab.arturbosch.detekt.api.Debt
+import io.gitlab.arturbosch.detekt.api.Entity
+import io.gitlab.arturbosch.detekt.api.Issue
+import io.gitlab.arturbosch.detekt.api.Rule
+import io.gitlab.arturbosch.detekt.api.Severity
+import io.gitlab.arturbosch.detekt.rules.hasAnnotation
+import org.jetbrains.kotlin.psi.KtClassOrObject
+
+class UnnecesaryNested(config: Config) : Rule(config) {
+  override val issue = Issue(
+    javaClass.simpleName,
+    Severity.CodeSmell,
+    "A @Nested class should always contain at least one @Test or more than one @Nested. And class shouldn't only contain one @Nested",
+    Debt.FIVE_MINS,
+  )
+
+  override fun visitClassOrObject(classOrObject: KtClassOrObject) {
+    if (classOrObject.hasAnnotation("Nested")) {
+      val body = classOrObject.body
+      if (body == null || body.declarations.isEmpty()) {
+        report(
+          CodeSmell(
+            issue,
+            Entity.atName(classOrObject),
+            "The class ${classOrObject.name.orEmpty()} is empty so it doesn't need to be annotated with `@Nested`.",
+          )
+        )
+      }
+    }
+
+    super.visitClassOrObject(classOrObject)
+  }
+}
---
+++ src/test/kotlin/com/braisgabin/detekt/junit/UnnecesaryNestedTest.kt
@@ -0,0 +1,57 @@
+package com.braisgabin.detekt.junit
+
+import io.gitlab.arturbosch.detekt.api.Config
+import io.gitlab.arturbosch.detekt.rules.KotlinCoreEnvironmentTest
+import io.gitlab.arturbosch.detekt.test.compileAndLintWithContext
+import org.assertj.core.api.Assertions.assertThat
+import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
+import org.junit.jupiter.api.Test
+
+@KotlinCoreEnvironmentTest
+internal class UnnecesaryNestedTest(private val env: KotlinCoreEnvironment) {
+
+  @Test
+  fun `report an empty Nested`() {
+    val code = """
+      import org.junit.jupiter.api.Test
+      import org.junit.jupiter.api.Nested
+
+      class A {
+        @Nested
+        class B {
+        }
+
+        @Test
+        fun test() {
+        }
+      }
+      """
+
+    val findings = UnnecesaryNested(Config.empty).compileAndLintWithContext(env, code)
+    assertThat(findings).hasSize(1)
+  }
+
+  @Test
+  fun `doesn't report`() {
+    val code = """
+      import org.junit.jupiter.api.Test
+      import org.junit.jupiter.api.Nested
+
+      class A {
+        @Nested
+        class B {
+          @Test
+          fun test() {
+          }
+        }
+
+        @Test
+        fun test() {
+        }
+      }
+      """
+
+    val findings = UnnecesaryNested(Config.empty).compileAndLintWithContext(env, code)
+    assertThat(findings).isEmpty()
+  }
+}
